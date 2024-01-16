582a1a704f2e880386b5d1ec1cc09944fa8a4aaa
delsevilla <sevillarendel09@gmail.com>
2024-01-16 11:59:59 +0800

    Change transact service UI

--- ServiceApp.Client/Pages/Services/Transact.razor.cs
+++ ServiceApp.Client/Pages/Services/Transact.razor.cs
@@ -5,103 +5,63 @@ using ServiceApp.Shared;
 using ServiceApp.Shared.Model.Services.Accounting.SubServices;
 using ServiceApp.Client.Enum.StaticClass;
 using ServiceApp.Client.Utility;
+using ServiceApp.Shared.Enum;
 
 namespace ServiceApp.Client.Pages.Services
 {
     public partial class Transact
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string TinNumber { get; set; }
-        public string Address { get; set; }
-        private MudTextField<string> multilineReference;
+        private bool expanded = true;
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? TinNumber { get; set; }
+        public string? Address { get; set; }
+        public int ActivePanel { get; set; }
+        public decimal Amount { get; set; }
+        private MudTextField<string>? multilineReference;
         private HashSet<ServiceItemData> TreeItems { get; set; } = new HashSet<ServiceItemData>();
         private Accounting? Accounting { get; set; }
+        List<string> Services { get; set; }
+
+        private Dictionary<string, bool> expandedStates = new Dictionary<string, bool>();
+
+        private void OnExpandCollapseClick(string service)
+        {
+            if (expandedStates.ContainsKey(service))
+            {
+                expandedStates[service] = !expandedStates[service];
+            }
+            else
+            {
+                expandedStates.Add(service, true);
+            }
+        }
         private void OpenDialog()
         {
             var options = new DialogOptions { CloseOnEscapeKey = true };
             DialogService.Show<ClientDialog>("Clients", options);
         }
 
-        protected void CheckedChanged(ServiceItemData item)
+        protected void GoToTab(int tabIndex)
         {
-            this.Accounting = new Accounting();
-            item.IsChecked = !item.IsChecked;
-            if (item.HasChild)
-            {
-                foreach (ServiceItemData child in item.TreeItems)
-                {
-                    child.IsChecked = item.IsChecked;
-                }
-            }
-            if (item.Parent != null)
-            {
-                item.Parent.IsChecked = !item.Parent.TreeItems.Any(i => !i.IsChecked);
-            }
-
-            if (item.HasChild)
-            {
-                foreach (ServiceItemData child in item.TreeItems)
-                {
-                    if (child.IsChecked)
-                    {
-                        if(child.Text == SubServices.Filingoftaxes) {this.Accounting.FilingOfTaxes = new FilingOfTaxes(){};}
-                        if (child.Text == SubServices.BIRRegistration) {}
-                        if (child.Text == SubServices.ITRPreparation) {}
-                        if (child.Text == SubServices.DTIRegistration) {}
-                        if (child.Text == SubServices.SECRegistration) {}
-                    }
-                }
-            }
-            else
-            {
-
-            }
-
-            var i = TreeItems;
+            this.ActivePanel = tabIndex;
         }
 
         protected override void OnInitialized()
         {
-            List<ServiceItemData> treeItems = new List<ServiceItemData>()
+            this.Services = new List<string>()
             {
-                new ServiceItemData(MainServices.Accounting),
-                new ServiceItemData(MainServices.OtherServices),
-                new ServiceItemData(MainServices.PSA),
-                new ServiceItemData(MainServices.DFA),
-                new ServiceItemData(MainServices.NOTARY),
-                new ServiceItemData(MainServices.LTO),
-                new ServiceItemData(MainServices.Airline),
-                new ServiceItemData(MainServices.VISA),
-                new ServiceItemData(MainServices.Financial),
-                new ServiceItemData(MainServices.ATMPortable)
-             };
-
-            treeItems[0].AddChild(SubServices.Filingoftaxes);
-            treeItems[0].AddChild(SubServices.BIRRegistration);
-            treeItems[0].AddChild(SubServices.ITRPreparation);
-            treeItems[0].AddChild(SubServices.DTIRegistration);
-            treeItems[0].AddChild(SubServices.SECRegistration);
-            treeItems[1].AddChild(SubServices.TransferofTitle);
-            treeItems[1].AddChild(SubServices.PCABAssistance);
-            treeItems[1].AddChild(SubServices.NBIAssistance);
-            treeItems[1].AddChild(SubServices.Weddingmanagement);
-            treeItems[2].AddChild(SubServices.Cenomar);
-            treeItems[2].AddChild(SubServices.BirthCertificate);
-            treeItems[2].AddChild(SubServices.MarriageCertificate);
-            treeItems[2].AddChild(SubServices.DeathCertificate);
-            treeItems[3].AddChild(SubServices.PassportAssistance);
-            treeItems[3].AddChild(SubServices.LostPassport);
-
-            foreach(var item in treeItems)
-            {
-                TreeItems.Add(item);
-            }
-        }
-
-        private void Proceed()
-        {
-            NavigationManager.NavigateTo("/summary");
+                MainServices.Accounting,
+                MainServices.OtherServices,
+                MainServices.PSA,
+                MainServices.DFA,
+                MainServices.NOTARY,
+                MainServices.LTO,
+                MainServices.Airline,
+                MainServices.VISA,
+                MainServices.Financial,
+                MainServices.ATMPortable
+            };
         }
     }
 }
